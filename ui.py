08b047f916328c51424886316dda25328881a5d5
denisntlfj <qwe.denissmirnov@gmail.com>
2026-05-21 18:13:11 +0200

    Added merging with new data

--- ui.py
+++ ui.py
@@ -1,22 +1,19 @@
 import streamlit as st
 import lda
+import storage_handler
 
-data_exists = False
-data_is_correct = True
+st.write('st.session_state:', st.session_state)#debug
 
-with st.sidebar:
-	st.header('header')
-	data_file = st.file_uploader('choose file')
+#does master data exist
+#are there trained model and scaler?
+#
+#load data
+#
+#want to add data?
+#
+#retrain button, show new stats on execute
+#input patient genes
+#
+#return lda.predict
 
-	if data_file is not None:
-		st.button('retrain the model')
 
-if data_exists and data_is_correct:
-	st.write('nice and good data')
-
-	st.button('train the model')
-	st.button('predict')
-else:
-	st.write('bad or no data')
-
-st.write(lda.AdvancedLDA())
