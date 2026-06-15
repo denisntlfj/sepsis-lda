e274df28cad1670e24aafd08377ab78862eba514
denisntlfj <qwe.denissmirnov@gmail.com>
2026-06-15 15:10:55 +0200

    To add learning on new data

--- qda.py
+++ qda.py
@@ -1,36 +1,41 @@
 import pandas as pd
 import numpy as np
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.model_selection import LeaveOneOut
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
 
-df = pd.read_csv('sepsis_data.csv')
-#add sorting genes alphabetically and target at last
-print('db size: ',df.shape)
+
+CSV_PATH = 'data.csv'
+df = pd.read_csv(CSV_PATH)
+#print('db size: ',df.shape)
+
+X = df[['PLAC8', 'CEACAM4', 'LAMP1', 'PLA2G7', 'BETA']]#rewrite this to any given genes, sorted
+y = df['Target']
+
+scaler = StandardScaler()
+a = QuadraticDiscriminantAnalysis() 
+
 
 def Evaluate():
-	X = df[['PLAC8', 'CEACAM4', 'LAMP1', 'PLA2G7']]#rewrite this to any given genes, sorted
-	y = df['Target']
-	
 	y_true = []
 	y_pred = []
-	
 	#cross-validation should usually be k-fold but since we have a poor samplesize using Leave-One-Out for now
 	for train_idx, test_idx in LeaveOneOut().split(X):
 		X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
 		y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
 		
+
 		#scaler only trains on train!
-		scaler = StandardScaler()
 		X_train_scaled = scaler.fit_transform(X_train)
 		X_test_scaled = scaler.transform(X_test)
 		
-		lda = LinearDiscriminantAnalysis()
-		lda.fit(X_train_scaled, y_train)
+		#print('X_test: ', X_test, '\nX_test_scaled: ', X_test_scaled)
 		
+		a.fit(X_train_scaled, y_train)
 		#predicting our test unit
-		pred = lda.predict(X_test_scaled)[0]
+		pred = a.predict(X_test_scaled)[0]
 		
 		y_pred.append(pred)
 		y_true.append(y_test.iloc[0])
@@ -38,5 +43,12 @@ def Evaluate():
 	return classification_report(y_true, y_pred, target_names=['Сепсис', 'Не сепсис'])
 
 
-def Predict():
-	pass
+def Predict(tested_patient):
+	X_tested = pd.DataFrame(tested_patient, index = [0])
+	X_train_scaled = scaler.fit_transform(X)
+	test_scaled = scaler.transform(X_tested)
+	pred = a.predict(test_scaled)[0]
+#	prob = a.predict_proba(test_scaled)[0]
+	return pred
+
+#print(Evaluate())
--- ui.py
+++ ui.py
@@ -1,19 +1,36 @@
 import streamlit as st
-import lda
-import storage_handler
+import qda
+#import storage_handler
+import os
+import sys
+import streamlit as st
 
-st.write('st.session_state:', st.session_state)#debug
+# Определяем, запущена ли программа как .exe или как обычный скрипт
+if getattr(sys, 'frozen', False):
+    BASE_DIR = os.path.dirname(sys.executable)
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-#does master data exist
-#are there trained model and scaler?
-#
-#load data
-#
-#want to add data?
-#
-#retrain button, show new stats on execute
-#input patient genes
-#
-#return lda.predict
+# Универсальный путь к твоей базе данных
+CSV_PATH = os.path.join(BASE_DIR, "sepsis_data.csv")
+
+#st.write('st.session_state:', st.session_state)#debug
+
+if st.toggle('Показать текущий отчёт модели'):
+    st.code(qda.Evaluate())
+
+st.markdown('#')
+
+st.markdown('Ввод данных тестируемого пациента:')
+tested_patient = {'PLAC8':232707.0, 'CEACAM4':2780.0, 'LAMP1':159.0, 'PLA2G7':0, 'BETA':2519837.0}
+
+for key in tested_patient:
+    tested_patient[key] = st.number_input(f'{key}:',step=1)
 
 
+if st.button('Спрогнозировать'):
+    prediction = qda.Predict(tested_patient)
+    if prediction == 1:
+        st.write('Прогноз: **Сепсис**')
+    else:
+        st.write('Прогноз: **Не сепсис**')
