dc139cfc2917e49e1e15103021e25bcd1b8ea4b3
denisntlfj <qwe.denissmirnov@gmail.com>
2026-07-15 15:58:16 +0200

    it works

--- run_app.py
+++ run_app.py
@@ -5,9 +5,7 @@ import streamlit.web.cli as stcli
 import webview
 
 def run_streamlit():
-    # Указываем путь к интерфейсу ui.py, который лежит внутри сборки
     if getattr(sys, 'frozen', False):
-        # Если запущено как .exe, берем из временной папки сборки
         base_dir = sys._MEIPASS
     else:
         base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -24,14 +22,11 @@ def run_streamlit():
     stcli.main()
 
 if __name__ == "__main__":
-    # Защита мультипроцессорности для скомпилированных программ
     multiprocessing.freeze_support()
     
-    # Запускаем локальный веб-сервер Streamlit в фоновом процессе
     p = multiprocessing.Process(target=run_streamlit)
     p.start()
     
-    # Открываем чистое окно программы без браузерных строк адреса
     try:
         webview.create_window(
             "Система диагностики QDA", 
@@ -41,6 +36,5 @@ if __name__ == "__main__":
         )
         webview.start()
     finally:
-        # При закрытии окна принудительно гасим сервер Streamlit
         p.terminate()
         p.join()
